1eba91f619e06d7c80720f6786cfe000191605f3
Jasper van Rooijen <jasper.van.rooijen@hotmail.com>
2015-10-08 18:41:00 +0200

    added stub classes

--- src/Date.java
+++ src/Date.java
@@ -2,4 +2,12 @@
  * Created by Jasper on 8-10-2015.
  */
 public class Date {
+    private String date;
+
+    public Date(String date) {
+
+    }
+    public String toString() {
+        return null;
+    }
 }
--- src/DateSet.java
+++ src/DateSet.java
@@ -1,5 +1,24 @@
+import java.util.ArrayList;
+
 /**
  * Created by Jasper on 8-10-2015.
  */
 public class DateSet {
+    private ArrayList<Date> dateSet;
+
+    public DateSet() {
+
+    }
+    public void add(Date date) {
+
+    }
+    public boolean contains(Date date) {
+        return false;
+    }
+    public DateSet intersection(DateSet comparedTo) {
+        return null;
+    }
+    public String toString() {
+        return null;
+    }
 }
