1ed2f47f8f064ff1fc792446ccd7b574d19ad3fd
Jasper van Rooijen <jasper.van.rooijen@hotmail.com>
2015-10-08 20:56:47 +0200

    finished

--- src/Date.java
+++ src/Date.java
@@ -5,9 +5,10 @@ public class Date {
     private String date;
 
     public Date(String date) {
-
+        this.date = date;
     }
+
     public String toString() {
-        return null;
+        return date;
     }
 }
--- src/DateSet.java
+++ src/DateSet.java
@@ -1,3 +1,4 @@
+import javax.xml.datatype.DatatypeConfigurationException;
 import java.util.ArrayList;
 
 /**
@@ -7,18 +8,50 @@ public class DateSet {
     private ArrayList<Date> dateSet;
 
     public DateSet() {
-
+        dateSet = new ArrayList<Date>();
     }
     public void add(Date date) {
-
+        dateSet.add(date);
     }
     public boolean contains(Date date) {
+        for (Date dt : dateSet) {
+            if (dt.equals(date)) {
+                return true;
+            }
+        }
         return false;
     }
     public DateSet intersection(DateSet comparedTo) {
-        return null;
+        DateSet newSet = new DateSet();
+        for(int i = 0; i < dateSet.size(); i++) {
+            Date originalDate = dateSet.get(i);
+            if (comparedTo.contains(originalDate)) {
+                newSet.add(originalDate);
+            }
+        }
+        return newSet;
     }
     public String toString() {
-        return null;
+        String result = ("Dateset {\n");
+        for(int i = 0; i < dateSet.size(); i++) {
+            result += (dateSet.get(i).toString()) + "\n";
+        }
+        result += ("}");
+        return result;
+    }
+    public static void main(String[] args) {
+        Date date = new Date("08-10-2015");
+        Date date2 = new Date("09-10-2015");
+        Date date3 = new Date("10-10-2015");
+        DateSet dateset = new DateSet();
+        DateSet dateset2 = new DateSet();
+        dateset.add(date);
+        dateset.add(date2);
+        dateset.add(date3);
+
+        dateset2.add(date);
+        dateset2.add(date2);
+
+        System.out.println(dateset.intersection(dateset2).toString());
     }
 }
